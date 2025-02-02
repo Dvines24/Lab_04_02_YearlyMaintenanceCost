d1e03a5554dbec3a5b6c0cc1bb38a2d41ecc2297
Dylan Vines <vinesdn@mail.uc.edu>
2025-02-01 21:56:10 -0500

    Completed Lab_04_02

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-package com.company;
-
 public class Main {
     public static void main(String[] args) {
-        
+        int springMaintCost = 250;
+        int summerMaintCost = 300;
+        int fallMaintCost = 225;
+        int winterMaintCost = 250;
+        int totalMaintCost = springMaintCost + summerMaintCost + fallMaintCost + winterMaintCost;
+        System.out.println("Your maintenance in the spring was " + springMaintCost + ". Your maintenance cost in the summer was " + summerMaintCost + ". Your maintenance cost in the fall was " + fallMaintCost + ".");
+        System.out.println("Your maintenance cost in the winter was " + winterMaintCost + ". Your total cost for maintenance this year was " + totalMaintCost + ".");
     }
 }
